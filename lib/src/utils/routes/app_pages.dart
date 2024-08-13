06168931d7fc1184b355ae4518031d906c67c974
subhan-ET <subhan@emergtechsolutions.com>
2024-08-13 12:01:43 +0500

    skelton

---
+++ lib/src/utils/routes/app_pages.dart
@@ -0,0 +1,8 @@
+
+import 'package:doctor_app/src/utils/routes/routes.dart';
+import 'package:get/route_manager.dart';
+
+class AppPages {
+
+  static const String INITIAL = Routes.splashScreen;
+  static final List<GetPage<dynamic>> routes = [];}
